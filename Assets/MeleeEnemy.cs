cdf9602dad527f4e7ca2085c53d7af4337df8e30
Evander-Jose <jemandexistiert@gmail.com>
2021-06-20 14:53:37 +0700

    Melee Enemy Added
    Melee enemies are added into the game. The only way to defeat these melee enemies is to reflect projectiles from ranged enemies into these enemies.

---
+++ Assets/MeleeEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class MeleeEnemy : MonoBehaviour
+{
+    public Enemy enemy;
+    public float damageAmount;
+    private Health playerHealth;
+    private Animator anim;
+    private void Start()
+    {
+        enemy = GetComponent<Enemy>();
+        playerHealth = enemy.playerTransform.GetComponent<Health>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float distanceFromPlayer = Vector3.Distance(transform.position, enemy.playerTransform.position);
+        if(distanceFromPlayer >= enemy.attackingRange)
+        {
+            Vector3 movDir = enemy.playerTransform.position - transform.position;
+            transform.Translate(movDir * Time.deltaTime);
+            anim.SetBool("isAttacking", false);
+        } else
+        {
+            //Do an attacking animation:
+            anim.SetBool("isAttacking", true);
+        }
+    }
+
+    public void AttackPlayer()
+    {
+        playerHealth.Damage(damageAmount);
+    }
+}
