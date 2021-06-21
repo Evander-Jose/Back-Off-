f194def04affcc88dac4b0d22dafe7b072ed1a1d
Evander-Jose <jemandexistiert@gmail.com>
2021-06-21 08:24:05 +0700

    Some Quality of Life Improvements
    The player will never leave the screen. (Clamped its position within the screen.)
    
    The health bar now flashes when health is low.

---
+++ Assets/PlayerBorder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBorder : MonoBehaviour
+{
+    public Transform minCorner;
+    public Transform maxCorner;
+    [Space]
+    public Transform playerTransform;
+
+    private void Start()
+    {
+        playerTransform = FindObjectOfType<PlayerInput>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float clamped_x = Mathf.Clamp(playerTransform.position.x, minCorner.position.x, maxCorner.position.x);
+        float clamped_y = Mathf.Clamp(playerTransform.position.y, minCorner.position.y, maxCorner.position.y);
+
+        playerTransform.position = new Vector3(clamped_x, clamped_y, playerTransform.position.z);
+    }
+}
--- Assets/Scripts/PlayerHealthBar.cs
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -9,11 +9,21 @@ public class PlayerHealthBar : MonoBehaviour
     public Slider slider;
     public FloatVariable currentHealth;
     public FloatVariable maxHealth;
+    [Space]
+    [Range(0f, 1f)] public float flashPercentage;
+    public float flashRate;
+    public Color flashColor1;
+    public Color flashColor2;
+    public Image fillBar;
 
     private void LateUpdate()
-    {
-        
+    {        
         //Debug.Log(percentage);
-        slider.value = currentHealth.CurrentValue / maxHealth.CurrentValue; ;
+        slider.value = currentHealth.CurrentValue / maxHealth.CurrentValue;
+
+        if(flashPercentage > slider.value)
+        {
+            fillBar.color = Color.Lerp(flashColor1, flashColor2, Mathf.Sin(flashRate*Time.time));
+        }
     }
 }
