818a97c1edd906c2250a19e6713be91d8bd5af3d
Evander-Jose <jemandexistiert@gmail.com>
2021-06-21 09:26:54 +0700

    Enemy Spawning Progression Added
    As the player kills more enemies, different types of enemies will spawn.
    
    This is determined by the ProgressionManager script. All this script does is compare the current score to the milestones, and then invoke the corresponding Unity Event.

---
+++ Assets/ProgressionManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class ProgressionManager : MonoBehaviour
+{
+    public IntVariable currentScore;
+    public List<ProgressionMilestone> milestones = new List<ProgressionMilestone>();
+
+    private void Update()
+    {
+        StartCoroutine(CheckAndUpdateProgressionMilestones());
+    }
+
+    private IEnumerator CheckAndUpdateProgressionMilestones()
+    {
+        for(int i = 0; i < milestones.Count; i++)
+        {
+            if(!milestones[i].milestoneCrossed)
+            {
+                if(milestones[i].milestone < currentScore.CurrentValue)
+                {
+                    //Mark the mileston as crossed:
+                    milestones[i].milestoneCrossed = true;
+                    milestones[i].onMilestoneCrossed.Invoke();
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
+
+[System.Serializable]
+public class ProgressionMilestone
+{
+    public UnityEvent onMilestoneCrossed;
+    public int milestone;
+    public bool milestoneCrossed;
+}
